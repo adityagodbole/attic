3cf3b462f6c1e0600431224ae5a69ba9d8799917
Aditya Godbole <code.aa@gdbl.me>
2022-01-29 16:53:27 +0530

    add cmake conan integration

--- cmake/auto.cmake
+++ cmake/auto.cmake
@@ -78,7 +78,24 @@ function(simple_build dir type)
 	endif()
 endfunction()
 
-function(autobuild)
+function(autobuild deps)
+	if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+		message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+		file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
+				"${CMAKE_BINARY_DIR}/conan.cmake"
+				EXPECTED_HASH SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
+				TLS_VERIFY ON)
+	endif()
+	set(_GLIBCXX_USE_CXX11_ABI 1)
+	include(${CMAKE_BINARY_DIR}/conan.cmake)
+	conan_cmake_configure(REQUIRES ${deps}
+			GENERATORS cmake_find_package)
+
+	conan_cmake_autodetect(settings)
+	conan_cmake_install(PATH_OR_REFERENCE .
+			BUILD missing
+			REMOTE conancenter
+			SETTINGS ${settings})
 	SUBDIRLIST(SUBDIRS "${PROJECT_SOURCE_DIR}/lib")
 	foreach(subdir ${SUBDIRS})
 		message(STATUS "Building lib ${subdir}")
@@ -99,5 +116,6 @@ macro(use_gtest)
 	)
 	FetchContent_MakeAvailable(googletest)
 	include(GoogleTest)
+	enable_testing()
 	enable_testing()
 endmacro()
